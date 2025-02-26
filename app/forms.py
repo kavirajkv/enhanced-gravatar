623868b05c8af159611498c497ce2596c988d103
kavirajkv <kavirajk36kv@gmail.com>
2025-02-27 02:02:22 +0530

    flask forms to get user data added

--- app/forms.py
+++ app/forms.py
@@ -4,10 +4,14 @@ from wtforms.validators import DataRequired, Email, Length
 
 
 class UserForm(FlaskForm):
-    email=StringField('Email', validators=[DataRequired(), Email()])
+    email=StringField('Email', validators=[DataRequired(), Email()],render_kw={'placeholder':'abcd@mail.com'})
     name=StringField('Full Name',validators=[DataRequired()])
     phone=StringField('Phone', validators=[DataRequired(), Length(min=10, max=15)])
     location=StringField('Location (City, Country)', validators=[DataRequired()])
     personalurl=StringField('Personal/Social URL',validators=[DataRequired()]),
     bio=TextAreaField('Bio', validators=[DataRequired(), Length(max=200)])
+    submit = SubmitField('Submit')
+    
+class SearchForm(FlaskForm):
+    email=StringField('Email', validators=[DataRequired(), Email()],render_kw={'placeholder':'abcd@mail.com'})
     submit = SubmitField('Submit')
