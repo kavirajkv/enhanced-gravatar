2c41a514df1460173eb4e4a914323460b00c5cb5
kavirajkv <kavirajk36kv@gmail.com>
2025-02-27 05:11:32 +0530

    requirements.txt and forms changed

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import redis
 
-app = Flask(__name__)
-app.secret_key = 'kavi1234' 
+app=Flask(__name__)
+app.secret_key ='kavi1234' 
+
+redisconnect=redis.Redis(host='localhost', port=6379)
 
 from app import views
+
+from app import api
+
+from app import forms
--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,7 @@ class UserForm(FlaskForm):
     name=StringField('Full Name',validators=[DataRequired()])
     phone=StringField('Phone', validators=[DataRequired(), Length(min=10, max=15)])
     location=StringField('Location (City, Country)', validators=[DataRequired()])
-    personalurl=StringField('Personal/Social URL',validators=[DataRequired()]),
+    personalurl=StringField('Personal/Social URL',validators=[DataRequired()])
     bio=TextAreaField('Bio', validators=[DataRequired(), Length(max=200)])
     submit = SubmitField('Submit')
     
